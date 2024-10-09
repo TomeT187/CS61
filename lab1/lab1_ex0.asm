61790f023759cb6d19490d7e7076a741b10e259d
TomeT187 <tomet187@gmail.com>
2024-10-09 09:57:23 -0700

    lab 1

---
+++ lab1/lab1_ex0.asm
@@ -0,0 +1,22 @@
+;=================================================
+; Name: Thomas Miko
+; Email: tmiko001@ucr.edu
+; 
+; Lab: lab 1, ex 0
+; Lab section: 21
+; TA: 
+; 
+;=================================================
+
+.orig x3000
+
+;Instructions
+LEA R0, MSG
+PUTS
+
+HALT
+
+;Local Data
+MSG .STRINGZ "Hello World!!!\n"
+
+.end
